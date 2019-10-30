bfba30d45f0ad098322d64f9ac9ba2f4a5be504d
korsicheg <korsicheg@gmail.com>
2019-10-30 13:26:09 +0200

    V1.0 - Cosmetics

--- src/gr/athtech/ioo/Main.java
+++ src/gr/athtech/ioo/Main.java
@@ -2,12 +2,12 @@ package gr.athtech.ioo;
 
 public class Main {
 
-    public static void main(String[] args) {
-	Flight f1 = new Flight();
-	f1.setArAirport("ATH");
-	f1.setDepAirport("DME");
-	System.out.println(f1.getArAirport());
-	System.out.println(f1.getDepAirport());
-	System.out.println(f1);
-    }
+	public static void main(String[] args) {
+		Flight f1 = new Flight();
+		f1.setArAirport("ATH");
+		f1.setDepAirport("DME");
+		System.out.println("From: " + f1.getArAirport());
+		System.out.println("To: " + f1.getDepAirport());
+		System.out.println(f1);
+	}
 }
